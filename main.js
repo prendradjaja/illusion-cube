7b84f77e9354082ed881c53e15255e3baca2c6b6
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-07-11 13:35:33 -0500

    A few more moves and demos

--- main.js
+++ main.js
@@ -25,10 +25,12 @@ function main() {
 
   setupControls();
 
-  setupCycleMakerDevtool();
+  // setupCycleMakerDevtool();
   // setupPaintDevtool();
 
   // suneDemo();
+  // moreDemosCube1();
+  // moreDemosCube0();
 }
 
 function drawCube() {
@@ -85,6 +87,45 @@ function suneDemo() {
   makeMove(1, 'R');
   makeMove(1, 'U'); makeMove(1, 'U');
   makeMove(1, 'R'); makeMove(1, 'R'); makeMove(1, 'R');
+}
+
+function moreDemosCube0() {
+
+  // R D R' D'
+  makeMove(0, 'R');
+  makeMove(0, 'D');
+  makeMove(0, 'R'); makeMove(0, 'R'); makeMove(0, 'R');
+  makeMove(0, 'D'); makeMove(0, 'D'); makeMove(0, 'D');
+
+}
+
+function moreDemosCube1() {
+
+  // // F R U R' U' F'
+  // makeMove(1, 'F');
+  // makeMove(1, 'R');
+  // makeMove(1, 'U');
+  // makeMove(1, 'R'); makeMove(1, 'R'); makeMove(1, 'R');
+  // makeMove(1, 'U'); makeMove(1, 'U'); makeMove(1, 'U');
+  // makeMove(1, 'F'); makeMove(1, 'F'); makeMove(1, 'F');
+
+  // // F R U R' U' F'
+  // makeMove(1, 'F');
+  // makeMove(1, 'R');
+  // makeMove(1, 'U');
+  // makeMove(1, 'R'); makeMove(1, 'R'); makeMove(1, 'R');
+  // makeMove(1, 'U'); makeMove(1, 'U'); makeMove(1, 'U');
+  // makeMove(1, 'F'); makeMove(1, 'F'); makeMove(1, 'F');
+
+  // Lefty sune (L' U' L U' L' U2 L)
+  makeMove(1, 'L'); makeMove(1, 'L'); makeMove(1, 'L');
+  makeMove(1, 'U'); makeMove(1, 'U'); makeMove(1, 'U');
+  makeMove(1, 'L');
+  makeMove(1, 'U'); makeMove(1, 'U'); makeMove(1, 'U');
+  makeMove(1, 'L'); makeMove(1, 'L'); makeMove(1, 'L');
+  makeMove(1, 'U'); makeMove(1, 'U');
+  makeMove(1, 'L');
+
 }
 
 function setupCycleMakerDevtool() {
