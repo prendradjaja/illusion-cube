ff4b53bd2870c923238ad0d1575894480bb51466
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-07-11 15:59:14 -0500

    move-definitions: Finish cube 1

--- more-demos.js
+++ more-demos.js
@@ -41,12 +41,28 @@ function moreDemosCube1() {
   // makeMove(1, 'R'); makeMove(1, 'R'); makeMove(1, 'R');
   // makeMove(1, 'D'); makeMove(1, 'D'); makeMove(1, 'D');
 
-  // L B D B' D' L'
-  makeMove(1, 'L');
-  makeMove(1, 'B');
-  makeMove(1, 'D');
-  makeMove(1, 'B'); makeMove(1, 'B'); makeMove(1, 'B');
-  makeMove(1, 'D'); makeMove(1, 'D'); makeMove(1, 'D');
-  makeMove(1, 'L'); makeMove(1, 'L'); makeMove(1, 'L');
+  // // L B D B' D' L'
+  // makeMove(1, 'L');
+  // makeMove(1, 'B');
+  // makeMove(1, 'D');
+  // makeMove(1, 'B'); makeMove(1, 'B'); makeMove(1, 'B');
+  // makeMove(1, 'D'); makeMove(1, 'D'); makeMove(1, 'D');
+  // makeMove(1, 'L'); makeMove(1, 'L'); makeMove(1, 'L');
+
+  // // MU U perm
+  // // M2 U M U2 M' U M2
+  // makeMove(1, 'M'); makeMove(1, 'M');
+  // makeMove(1, 'U');
+  // makeMove(1, 'M');
+  // makeMove(1, 'U'); makeMove(1, 'U');
+  // makeMove(1, 'M'); makeMove(1, 'M'); makeMove(1, 'M');
+  // makeMove(1, 'U');
+  // makeMove(1, 'M'); makeMove(1, 'M');
+
+  // // E S E' S'
+  // makeMove(1, 'E');
+  // makeMove(1, 'S');
+  // makeMove(1, 'E'); makeMove(1, 'E'); makeMove(1, 'E');
+  // makeMove(1, 'S'); makeMove(1, 'S'); makeMove(1, 'S');
 
 }
